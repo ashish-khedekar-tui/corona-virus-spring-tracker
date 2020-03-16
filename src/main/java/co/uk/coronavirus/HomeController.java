26b92172e9783f63c4b4aa19e57209ed98dbc8c3
Ashish Khedekar <ashish.khedekar@tui.co.uk>
2020-03-16 15:45:45 +0000

    Added favicon file

--- src/main/java/co/uk/coronavirus/HomeController.java
+++ src/main/java/co/uk/coronavirus/HomeController.java
@@ -1,10 +1,14 @@
 package co.uk.coronavirus;
 
+import co.uk.coronavirus.models.LocationStats;
 import co.uk.coronavirus.services.CoronaVirusTrackerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Optional;
+import java.util.Set;
 
 @Controller
 public class HomeController
@@ -17,7 +21,14 @@ public class HomeController
    @GetMapping("/")
    public String home(final Model model)
    {
-      model.addAttribute("locationStats", coronaVirusTrackerService.getLocationStatsList());
+      final Set<LocationStats> locationStatsList = coronaVirusTrackerService.getLocationStatsList();
+      model.addAttribute("locationStats", locationStatsList);
+
+      final Optional<LocationStats> first = locationStatsList.stream().findFirst();
+
+      first.ifPresent(e ->
+               model.addAttribute("worstAffectedArea", String.format("%s - %s = Total cases reported yesterday - %d ", e.getState(), e.getCountry(), e.getCasesReportedInLastDay())));
+
       return "home";
    }
 
